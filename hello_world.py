113f2aaf028fd4db5c1aae34efe4cec54f8d4710
vallabhmg <vallabhgorule2005@gmail.com>
2026-01-17 14:03:41 +0530

    Completed 1st Task.In this i done with print , time module and comment

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+import datetime 
+
+'''
+Introduction mySelf
+'''
+
+print("Name=Vallabh Gorule")
+print("Pyhton Virtual Intership of 3 months by Elevate Lab")
+checknow=datetime.date.today()
+print("Code written date Today Date= 17/01/2026 and Code Checking Date:",checknow)
+
+'''Checker enquriy Form'''
+
+print("Enter Name of Cheking Code:")
+profName=input()
+print("Enter Role(eg manager,assistant):")
+profRole=input()
+print("Thankyou, ",profName," Sir To Check my project.\nYour are A good ",profRole)
